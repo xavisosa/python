68a1a6cbc66c54f217db98c93b09f60403f37cb6
Xavier Sosa <xavisosa@gmail.com>
2022-03-21 19:33:38 -0300

    Creamos nuestra primera app de python

---
+++ main.py
@@ -0,0 +1,8 @@
+# CREAMOS LA FUNCION APP QUE INICIA MI PROGAMA
+def app():
+    print("Hola Mundo!")
+
+# CREAMOS NUESTRO PUNTO PARTIDA
+if __name__ == "__main__":
+    print('Iniciando programa...')
+    app()
