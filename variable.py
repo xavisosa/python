74c7ccbecf7c3536dba1eaa4a3dc99ce6860af61
Xavier Sosa <xavisosa@gmail.com>
2022-03-28 19:17:42 -0300

    Crear variales e imprimirlas

---
+++ variable.py
@@ -0,0 +1,51 @@
+# CREAR 3 VARIABLES ENTERAS
+num_1 = 10
+num_2 = 44
+num_3 = 9393
+
+# IMPRIMIR EL VALOR DE LAS VARIABLES Y SU TIPO DE DATO
+print('La variale num1 tiene el valor',num_1)
+print('Su tipo de dato es',type(num_1))
+print('La variale num2 tiene el valor',num_2)
+print('Su tipo de dato es',type(num_2))
+print('La variale num3 tiene el valor',num_3)
+print('Su tipo de dato es',type(num_3))
+
+print('-'*50)
+
+# CREAR 3 VARIABLES DECIMALES
+num_dec_1 = 10.9
+num_dec_2 = 44.34
+num_dec_3 = 9393.1
+
+# IMPRIMIR EL VALOR DE LAS VARIABLES Y SU TIPO DE DATO
+print('La variale num1 tiene el valor',num_dec_1)
+print('Su tipo de dato es',type(num_dec_1))
+print('La variale num2 tiene el valor',num_dec_2)
+print('Su tipo de dato es',type(num_dec_2))
+print('La variale num3 tiene el valor',num_dec_3)
+print('Su tipo de dato es',type(num_dec_3))
+
+print('-'*50)
+
+# CREAR 3 VARIABLES DECIMALES
+bool_1 = True
+bool_2 = False
+
+# IMPRIMIR EL VALOR DE LAS VARIABLES Y SU TIPO DE DATO
+print('La variale bool1 tiene el valor',bool_1)
+print('Su tipo de dato es',type(bool_1))
+print('La variale bool2 tiene el valor',bool_2)
+print('Su tipo de dato es',type(bool_2))
+
+print('-'*50)
+
+# CREAR 3 VARIABLES DECIMALES
+nula_1 = None
+nula_2 = None
+
+# IMPRIMIR EL VALOR DE LAS VARIABLES Y SU TIPO DE DATO
+print('La variale nula1 tiene el valor',nula_1)
+print('Su tipo de dato es',type(nula_1))
+print('La variale nula2 tiene el valor',nula_2)
+print('Su tipo de dato es',type(nula_2))
